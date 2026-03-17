bdf2e176fd15a173e8ff977887c97986614abfe8
Juanma Hidalgo <juanma06@gmail.com>
2026-03-17 12:24:35 +0100

    feat: update jump URL (#2613)
    * feat: update jump URL
    
    * feat: buildExplorerUrl uses config to get explorer url

--- webapp/src/modules/nft/parcel/utils.ts
+++ webapp/src/modules/nft/parcel/utils.ts
@@ -1,7 +1,9 @@
+import { config } from '../../../config'
+
 export function getId(x: number | string, y: number | string) {
   return x + ',' + y
 }
 
 export function buildExplorerUrl(x: string | number, y: string | number) {
-  return `https://play.decentraland.org/?position=${x},${y}`
+  return `${config.get('EXPLORER_URL')}/?position=${x},${y}`
 }
