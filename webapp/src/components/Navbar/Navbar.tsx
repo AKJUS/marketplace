a5ad8cdc666849b406e4f173f4c28074c760369b
Juanma Hidalgo <juanma06@gmail.com>
2026-04-09 12:18:38 +0200

    feat: introduce new navbar (#2616)
    * feat: introduce new navbar
    
    * feat: remove onCreditsClick handler
    
    * fix: eslint issue
    
    * feat: use encodeURIComponent
    
    * feat: delete navbar

--- webapp/src/components/Navbar/Navbar.tsx
+++ webapp/src/components/Navbar/Navbar.tsx
@@ -1,40 +1,23 @@
 import React, { useCallback } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { Navbar2 as BaseNavbar2 } from 'decentraland-dapps/dist/containers/Navbar'
-import { NavbarPages } from 'decentraland-ui/dist/components/Navbar/Navbar.types'
 import { config } from '../../config'
-import { locations } from '../../modules/routing/locations'
 import { Props } from './Navbar.types'
-import './Navbar.css'
 
 const Navbar = (props: Props) => {
   const { pathname, search } = useLocation()
-  const history = useHistory()
 
   const handleOnSignIn = useCallback(() => {
     const searchParams = new URLSearchParams(search)
     const currentRedirectTo = searchParams.get('redirectTo')
-    const basename = /^decentraland.(zone|org|today)$/.test(window.location.host) ? '/marketplace' : ''
+    const basename = /^decentraland\.(zone|org|today)$/.test(window.location.host) ? '/marketplace' : ''
     const redirectTo = !currentRedirectTo ? `${basename}${pathname}${search}` : `${basename}${currentRedirectTo}`
 
-    window.location.replace(`${config.get('AUTH_URL')}/login?redirectTo=${redirectTo}`)
+    window.location.replace(`${config.get('AUTH_URL')}/login?redirectTo=${encodeURIComponent(redirectTo)}`)
   }, [pathname, search])
 
-  const handleOnClickAccount = useCallback(() => {
-    history.push(locations.settings())
-  }, [history])
-
   return (
-    <BaseNavbar2
-      {...props}
-      withChainSelector
-      withNotifications
-      activePage={NavbarPages.MARKETPLACE}
-      hasActivity={props.hasPendingTransactions}
-      identity={props.identity}
-      onSignIn={handleOnSignIn}
-      onClickMarketplaceAuthorization={handleOnClickAccount}
-    />
+    <BaseNavbar2 {...props} withChainSelector withNotifications activePage="shop" identity={props.identity} onSignIn={handleOnSignIn} />
   )
 }
 
--- webapp/src/components/Navbar/Navbar.types.ts
+++ webapp/src/components/Navbar/Navbar.types.ts
@@ -1,12 +1,7 @@
 import { AuthIdentity } from '@dcl/crypto'
-import { NavbarProps } from 'decentraland-ui/dist/components/Navbar/Navbar.types'
 
-export type Props = Partial<NavbarProps> & {
-  hasPendingTransactions: boolean
-  enablePartialSupportAlert?: boolean
+export type Props = {
   identity?: AuthIdentity
 }
 
-export type OwnProps = Pick<Props, 'enablePartialSupportAlert'>
-
-export type MapStateProps = Pick<Props, 'hasPendingTransactions' | 'identity'>
+export type MapStateProps = Pick<Props, 'identity'>
